c5d98cbe32950e45f89f50b3aac9dc97fc37a822
Datavizcowboy <graphicprototype@gmail.com>
2020-09-10 18:34:36 +0100

    Add color and shift absolute position
    Blue on the high hills fits well

--- Mountains/ridge.js
+++ Mountains/ridge.js
@@ -1,6 +1,6 @@
 //--------------------------- SVG definition
 
-var widthres = 980;
+var widthres = 1180;
 var heightres = 900;
 
 var svgres = d3.select("div.leftpolyresources-div")
@@ -39,7 +39,7 @@ d3.json("../DATA/opportunities.json", function (error, data) {
             var c = -0 + (i) * 70 + (d.query_count * .1) + "," + 420
             return (a + " " + b + " " + c);
         })
-        .style("fill", function (d, i) { return d3.rgb(200 - i * 6, 200 - i * 6, 200 - i * 6); })
+        .style("fill", function (d, i) { return d3.rgb(0 - i * 6, 250 - i * 2, 250 + i * 2); })
         .style("opacity", 1);
 
     mount2.selectAll('polygon').data(data.data).enter()
@@ -47,8 +47,8 @@ d3.json("../DATA/opportunities.json", function (error, data) {
         .attr("points", function (d, i) {
             var pitchbar = 1
             var a = -0 + (i) * 70 + "," + 420
-            var b = -0 + (i) * 70 + (d.click_count * .5) + "," + (420 - (d.click_count * 1))
-            var c = -0 + (i) * 70 + (d.click_count * 1) + "," + 420
+            var b = -0 + (i) * 70 + (d.click_count * .4) + "," + (420 - (d.click_count * .8))
+            var c = -0 + (i) * 70 + (d.click_count * .8) + "," + 420
             return (a + " " + b + " " + c);
         })
         .style("fill","#333")
@@ -68,10 +68,10 @@ d3.json("../DATA/opportunities.json", function (error, data) {
 
     mountlineclick.selectAll('line').data(data.data).enter()
         .append('line')
-        .attr("x1", function (d, i) { return -0 + (i) * 70 + (d.click_count * .5); })
-        .attr("y1", function (d) { return (412 - (d.click_count * 1)); })
-        .attr("x2", function (d, i) { return -0 + (i) * 70 + (d.click_count * .5); })
-        .attr("y2", function (d) { return (395 - (d.click_count * 1)); })
+        .attr("x1", function (d, i) { return -0 + (i) * 70 + (d.click_count * .4); })
+        .attr("y1", function (d) { return (412 - (d.click_count * .8)); })
+        .attr("x2", function (d, i) { return -0 + (i) * 70 + (d.click_count * .4); })
+        .attr("y2", function (d) { return (395 - (d.click_count * .8)); })
         .attr("stroke-width", 1)
         .style("opacity", 1)
         .attr("stroke", "#333");
@@ -98,8 +98,8 @@ d3.json("../DATA/opportunities.json", function (error, data) {
 
     mounttextclick.selectAll('text').data(data.data).enter()
         .append("text")
-        .attr("x", function (d, i) { return -2 + (i) * 70 + (d.click_count * .5); })
-        .attr("y", function (d) { return (390 - (d.click_count * 1)); })
+        .attr("x", function (d, i) { return -2 + (i) * 70 + (d.click_count * .4); })
+        .attr("y", function (d) { return (390 - (d.click_count * .8)); })
         .text(function (d) { return (d.click_count); })
         .attr("font-family", "Gill Sans, Century Gothic, sans-serif")
         .attr("font-size", label_size)
@@ -107,8 +107,8 @@ d3.json("../DATA/opportunities.json", function (error, data) {
 
     mounttextclick2.selectAll('text').data(data.data).enter()
         .append("text")
-        .attr("x", function (d, i) { return -2 + (i) * 70 + (d.click_count * .5); })
-        .attr("y", function (d) { return (380 - (d.click_count * 1)); })
+        .attr("x", function (d, i) { return -2 + (i) * 70 + (d.click_count * .4); })
+        .attr("y", function (d) { return (380 - (d.click_count * .8)); })
         .text(function (d, i) { return d.terms; })
         .attr("font-family", "Gill Sans, Century Gothic, sans-serif")
         .attr("font-size", label_size)
